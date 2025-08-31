b29897bc582572897066afd8853f729db0449cf8
DanH <scycer1@hotmail.com>
2025-08-31 23:34:36 +1000

    clean

--- src/app.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { RouterProvider, Router } from '@tanstack/react-router';
-import './styles.css';
-
-export function App({ router }: { router: Router<any, any> }) {
-  return <RouterProvider router={router} />;
-}
